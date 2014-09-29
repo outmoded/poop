f45fa8649685d80ed6415ad5f7825fe7aff55b72
Adam Bretz <arbretz@gmail.com>
2014-09-29 12:27:29 -0400

    Removed outer describe block

--- test/index.js
+++ test/index.js
@@ -21,87 +21,84 @@ var describe = lab.describe;
 var it = lab.it;
 
 
-describe('Poop', function () {
+var logPath = __dirname + '/test1.log';
+var server;
+before(function (done) {
 
-    var logPath = __dirname + '/test1.log';
-    var server;
-    before(function (done) {
+    server = new Hapi.Server();
+    server.pack.register({
+        plugin: require('../'),
+        options: { logPath: logPath }
+    }, function (err) {
 
-        server = new Hapi.Server();
-        server.pack.register({
+        expect(err).to.not.exist;
+        var other = new Hapi.Server();
+        other.pack.register({
             plugin: require('../'),
             options: { logPath: logPath }
         }, function (err) {
 
             expect(err).to.not.exist;
-            var other = new Hapi.Server();
-            other.pack.register({
-                plugin: require('../'),
-                options: { logPath: logPath }
-            }, function (err) {
-
-                expect(err).to.not.exist;
-                done();
-            });
+            done();
         });
     });
+});
 
-    after(function (done) {
+after(function (done) {
+
+    Fs.readdir(Path.join(__dirname, '..'), function (err, files) {
+
+        var heaps = 0;
+        for (var i = 0, il = files.length; i < il; ++i) {
+            if (files[i].indexOf('heapdump-') === 0) {
+                heaps++;
+                Fs.unlinkSync(Path.join(__dirname, '..', files[i]));
+            }
+        }
+
+        done();
+    });
+});
+
+it('can log uncaught exceptions to the file provided and exits process', function (done) {
+
+    var orig = process.exit;
+    process.exit = function (code) {
+
+        process.exit = orig;
+        expect(code).to.equal(1);
+        expect(Fs.existsSync(logPath));
+        Fs.unlinkSync(logPath);
+
+        done();
+    };
+
+    process.emit('uncaughtException', new Error('test'));
+});
+
+it('can handle SIGUSR1 events', function (done) {
+
+    setTimeout(function () {
+
+        process.emit('SIGUSR1');
 
         Fs.readdir(Path.join(__dirname, '..'), function (err, files) {
 
-            var heaps = 0;
+            var heapsAfter = 0;
             for (var i = 0, il = files.length; i < il; ++i) {
                 if (files[i].indexOf('heapdump-') === 0) {
-                    heaps++;
-                    Fs.unlinkSync(Path.join(__dirname, '..', files[i]));
+                    heapsAfter++;
                 }
             }
 
+            expect(heapsAfter).to.equal(2);
             done();
         });
-    });
+    }, 500);
+});
 
-    it('can log uncaught exceptions to the file provided and exits process', function (done) {
+it('can handle null options in register()', function (done) {
+    var Poop = require('../');
 
-        var orig = process.exit;
-        process.exit = function (code) {
-
-            process.exit = orig;
-            expect(code).to.equal(1);
-            expect(Fs.existsSync(logPath));
-            Fs.unlinkSync(logPath);
-
-            done();
-        };
-
-        process.emit('uncaughtException', new Error('test'));
-    });
-
-    it('can handle SIGUSR1 events', function (done) {
-
-        setTimeout(function () {
-
-            process.emit('SIGUSR1');
-
-            Fs.readdir(Path.join(__dirname, '..'), function (err, files) {
-
-                var heapsAfter = 0;
-                for (var i = 0, il = files.length; i < il; ++i) {
-                    if (files[i].indexOf('heapdump-') === 0) {
-                        heapsAfter++;
-                    }
-                }
-
-                expect(heapsAfter).to.equal(2);
-                done();
-            });
-        }, 500);
-    });
-
-    it('can handle null options in register()', function (done) {
-        var Poop = require('../');
-
-        Poop.register({}, null, done);
-    });
+    Poop.register({}, null, done);
 });
