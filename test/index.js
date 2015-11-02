0649e29d14ebc7d8f612f4d390f374d37219904b
cjihrig <cjihrig@gmail.com>
2015-11-02 08:49:10 -0500

    v2.0.0 - ES6 updates

--- test/index.js
+++ test/index.js
@@ -2,56 +2,56 @@
 
 // Load modules
 
-var Fs = require('fs');
-var Os = require('os');
-var Path = require('path');
-var Code = require('code');
-var Hapi = require('hapi');
-var Lab = require('lab');
-var Poop = require('../lib');
-var PoopUtils = require('../lib/utils');
+const Fs = require('fs');
+const Os = require('os');
+const Path = require('path');
+const Code = require('code');
+const Hapi = require('hapi');
+const Lab = require('lab');
+const Poop = require('../lib');
+const PoopUtils = require('../lib/utils');
 
 
 // Declare internals
 
-var internals = {
+const internals = {
     logPath: Path.join(__dirname, 'test.log')
 };
 
 
 // Test shortcuts
 
-var lab = exports.lab = Lab.script();
-var expect = Code.expect;
-var describe = lab.describe;
-var it = lab.it;
+const lab = exports.lab = Lab.script();
+const expect = Code.expect;
+const describe = lab.describe;
+const it = lab.it;
 
 
-describe('Poop', function () {
+describe('Poop', () => {
 
-    lab.afterEach(function (done) {
+    lab.afterEach((done) => {
 
         internals.cleanup(done);
     });
 
-    it('logs uncaught exceptions, takes dump, and exits process', function (done) {
+    it('logs uncaught exceptions, takes dump, and exits process', (done) => {
 
-        internals.prepareServer(function (err, server) {
+        internals.prepareServer((err, server) => {
 
-            var orig = process.exit;
+            const orig = process.exit;
 
             process.exit = function (code) {
 
                 process.exit = orig;
                 expect(code).to.equal(1);
 
-                var exception = JSON.parse(Fs.readFileSync(internals.logPath, 'utf8'));
+                const exception = JSON.parse(Fs.readFileSync(internals.logPath, 'utf8'));
 
                 expect(exception.message).to.equal('test');
                 expect(exception.stack).to.be.a.string();
                 expect(exception.timestamp).to.be.a.number();
                 Fs.unlinkSync(internals.logPath);
-                internals.countHeaps(function (err, count) {
+                internals.countHeaps((err, count) => {
 
                     expect(err).to.not.exist();
                     expect(count).to.equal(1);
@@ -63,13 +63,13 @@ describe('Poop', function () {
         });
     });
 
-    it('takes dump on SIGUSR1 events', function (done) {
+    it('takes dump on SIGUSR1 events', (done) => {
 
-        internals.prepareServer(function (err, server) {
+        internals.prepareServer((err, server) => {
 
             expect(err).to.not.exist();
             process.emit('SIGUSR1');
-            internals.countHeaps(function (err, count) {
+            internals.countHeaps((err, count) => {
 
                 expect(err).to.not.exist();
                 expect(count).to.equal(1);
@@ -78,22 +78,22 @@ describe('Poop', function () {
         });
     });
 
-    it('configures the the log file', function (done) {
+    it('configures the the log file', (done) => {
 
-        var err1 = new Error('test 1');
-        var err2 = new Error('test 2');
-        var options = {
+        const err1 = new Error('test 1');
+        const err2 = new Error('test 2');
+        const options = {
             logPath: Path.join(__dirname, 'config.log'),
             writeStreamOptions: { flags: 'a' }
         };
 
-        PoopUtils.log(err1, options, function () {
+        PoopUtils.log(err1, options, () => {
 
-            PoopUtils.log(err2, options, function () {
+            PoopUtils.log(err2, options, () => {
 
-                var exceptions = Fs.readFileSync(options.logPath, 'utf8').split(Os.EOL);
-                var ex1 = JSON.parse(exceptions[0]);
-                var ex2 = JSON.parse(exceptions[1]);
+                const exceptions = Fs.readFileSync(options.logPath, 'utf8').split(Os.EOL);
+                const ex1 = JSON.parse(exceptions[0]);
+                const ex2 = JSON.parse(exceptions[1]);
 
                 expect(ex1.message).to.equal('test 1');
                 expect(ex1.stack).to.be.a.string();
@@ -107,13 +107,13 @@ describe('Poop', function () {
         });
     });
 
-    it('can register the plugin multiple times', function (done) {
+    it('can register the plugin multiple times', (done) => {
 
-        internals.prepareServer(function (err, server) {
+        internals.prepareServer((err, server) => {
 
             expect(err).to.not.exist();
             expect(server).to.exist();
-            internals.prepareServer(function (err, other) {
+            internals.prepareServer((err, other) => {
 
                 expect(err).to.not.exist();
                 expect(other).to.exist();
@@ -122,9 +122,9 @@ describe('Poop', function () {
         });
     });
 
-    it('can handle null options in register()', function (done) {
+    it('can handle null options in register()', (done) => {
 
-        Poop.register({}, null, function (err) {
+        Poop.register({}, null, (err) => {
 
             expect(err).to.not.exist();
             done();
@@ -135,13 +135,13 @@ describe('Poop', function () {
 
 internals.prepareServer = function (callback) {
 
-    var server = new Hapi.Server();
+    const server = new Hapi.Server();
 
     server.connection();
     server.register({
         register: Poop,
         options: { logPath: internals.logPath }
-    }, function (err) {
+    }, (err) => {
 
         expect(err).to.not.exist();
         callback(null, server);
@@ -151,15 +151,15 @@ internals.prepareServer = function (callback) {
 
 internals.countHeaps = function (callback) {
 
-    Fs.readdir(process.cwd(), function (err, files) {
+    Fs.readdir(process.cwd(), (err, files) => {
 
         if (err) {
             return callback(err);
         }
 
-        var count = 0;
+        let count = 0;
 
-        for (var i = 0, il = files.length; i < il; ++i) {
+        for (let i = 0; i < files.length; ++i) {
             if (files[i].indexOf('heapdump-') === 0) {
                 count++;
             }
@@ -172,13 +172,13 @@ internals.countHeaps = function (callback) {
 
 internals.cleanup = function (callback) {
 
-    Fs.readdir(process.cwd(), function (err, files) {
+    Fs.readdir(process.cwd(), (err, files) => {
 
         if (err) {
             return callback(err);
         }
 
-        for (var i = 0, il = files.length; i < il; ++i) {
+        for (let i = 0; i < files.length; ++i) {
             if (files[i].indexOf('heapdump-') === 0) {
                 Fs.unlinkSync(Path.join(process.cwd(), files[i]));
             }
